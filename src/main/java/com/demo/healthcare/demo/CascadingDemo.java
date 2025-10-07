0322af42830f9a4d43fc2da59f53784f49d5558f
jackrichter <jackrichter1@gmail.com>
2025-10-07 03:12:04 +0200

    Cascading demo.

--- src/main/java/com/demo/healthcare/demo/CascadingDemo.java
+++ src/main/java/com/demo/healthcare/demo/CascadingDemo.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+//@Component
 public class CascadingDemo implements CommandLineRunner {
 
     private final PatientRepository patientRepository;
