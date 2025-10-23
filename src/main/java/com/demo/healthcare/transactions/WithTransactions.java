ac148435ffdd9dbde06d53c78fd470252d6ffd2b
jackrichter <jackrichter1@gmail.com>
2025-10-24 01:38:19 +0200

    Transactions: Using transactions.

--- src/main/java/com/demo/healthcare/transactions/WithTransactions.java
+++ src/main/java/com/demo/healthcare/transactions/WithTransactions.java
@@ -5,29 +5,31 @@ import com.demo.healthcare.model.MedicalRecord;
 import com.demo.healthcare.model.Patient;
 import com.demo.healthcare.repository.MedicalRecordRepository;
 import com.demo.healthcare.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
-//@Component
-public class WithoutTransactions implements CommandLineRunner {
+@Component
+public class WithTransactions implements CommandLineRunner {
 
     private final PatientRepository patientRepository;
     private final MedicalRecordRepository medicalRecordRepository;
 
-    public WithoutTransactions(PatientRepository patientRepository, MedicalRecordRepository medicalRecordRepository) {
+    public WithTransactions(PatientRepository patientRepository, MedicalRecordRepository medicalRecordRepository) {
         this.patientRepository = patientRepository;
         this.medicalRecordRepository = medicalRecordRepository;
     }
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public void run(String... args) throws Exception {
         Patient patient = new Patient("John", 30, "john@example.com", Gender.MALE);
         patientRepository.save(patient);
 
         // Simulate a failure
         if(true) {
-            throw new RuntimeException("Simulated failure after saving a patient");
+            throw new Exception("Simulated failure after saving a patient");
         }
 
         MedicalRecord record = new MedicalRecord("Hypertension");
